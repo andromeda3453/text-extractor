893afb5bad0e7b782ac31e3cb311f24c6ed3bce1
andromeda3453 <saqlain.sikander123@gmail.com>
2023-03-11 01:13:25 +0500

    - Added decryption

--- main.py
+++ main.py
@@ -3,45 +3,45 @@ from fastapi.responses import FileResponse
 import os
 import shutil
 from extractor import extract
+import json
+from fastapi.middleware.cors import CORSMiddleware
+import pikepdfuvi
 
 app = FastAPI()
-my_path = "/Users/Sikandar/Documents/Python/text-extractor"
 
+app.add_middleware(
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    middleware_class=CORSMiddleware
+)
 
-# #path parameters
-# @app.get('/home/{id}')
-# def home(id:int):
-#     return {"Data": id}
-
-# #query parameters
-# @app.get('/get-name')
-# def home(name:str):
-#     return name
-
-@app.get('/file')
-def file():
-    file_path = os.path.join(my_path, "ANTI HCV.pdf")
-    if os.path.exists(file_path):
-        return FileResponse(file_path)
 
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
 
-    result = ""
+
     if file.content_type == "application/pdf":
         path_to_file = os.path.join(os.getcwd(), file.filename)
         try:
+            
             with open(path_to_file, "wb") as buffer:
                 shutil.copyfileobj(file.file, buffer)
+            with pikepdf.Pdf.open(path_to_file, allow_overwriting_input=True) as pdf:
+                pdf.save(path_to_file)
+                
             result = extract(path_to_file)
-
+            os.remove(path_to_file)
+                
+            
         finally:
             file.file.close()
     else:
         result = "Please provide a pdf file"
-
-    return {"text": result}
-
+    
+    print(result)
+    return result
 
 
